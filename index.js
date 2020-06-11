f4f891ce86ff957774181d6225707287f63290d4
Scott Mittman <sm2884@nau.edu>
2020-06-11 21:05:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var arr=[];
+  let newstr;
+  for (let i=0;i<musicians.length;i++){
+    newstr=`${musicians[i]} plays ${instruments[i]}`
+    arr.push(newstr)
+  }
+  return arr
+}
