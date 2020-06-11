419a273ccf148bacf09f93bb6d3c0848e8c2ec94
Scott Mittman <sm2884@nau.edu>
2020-06-11 21:31:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var arr=[];
+  let newstr;
+  for (let i=0;i<musicians.length;i++){
+    newstr=`${musicians[i]} plays ${instruments[i]}`
+    arr.push(newstr)
+  }
+  return arr
+}
+function johnLennonFacts(facts){
+  let newstr;
+  let arr=[]
+  let i=0;
+  while (i<facts.length){
+    newstr=facts[i]+"!!!";
+    arr.push(newstr);
+    i++;
+  }
+  return arr
+}
+
+
+function iLoveTheBeatles(number){
+  var arr=[];
+  do {
+    arr.push("I love the Beatles!");
+    number+=1;
+  } while (number<15)
+  return arr
+}
